da6bf14194d0099beb6d117e968f5262a1392ebe
Navyroot <vyubbit@gmail.com>
2018-01-08 14:34:31 +0700

    bad souce not yet config

---
+++ node_modules/simpl-schema/dist/validation/allowedValuesValidator.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = allowedValuesValidator;
+
+var _SimpleSchema = require('../SimpleSchema');
+
+var _lodash = require('lodash.includes');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function allowedValuesValidator() {
+  if (!this.valueShouldBeChecked) return;
+
+  var allowedValues = this.definition.allowedValues;
+  if (!allowedValues) return;
+
+  var isAllowed = (0, _lodash2.default)(allowedValues, this.value);
+  return isAllowed ? true : _SimpleSchema.SimpleSchema.ErrorTypes.VALUE_NOT_ALLOWED;
+}
