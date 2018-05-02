55439f30a8694a8e906c64d50589f958c6d06a29
Navyroot <vyubbit@gmail.com>
2018-05-02 22:48:30 +0700

    refector form

--- imports/client/pages/Payment.vue
+++ imports/client/pages/Payment.vue
@@ -1,83 +1,141 @@
 <template>
   <div>
-    <component :is="currentModal"
+    <!-- <component :is="currentModal"
                :modal-doc="modalDoc"
                :visible="modalVisible"
                @modal-close="handleClose">
-    </component>
-    <fieldset style="padding-bottom:-5vh;">
-      <!-- <legend>Filter</legend> -->
-      <el-form :model="form"
-               ref="form"
-               :rules="rules"
+    </component> -->
+    <!-- <legend>Filter</legend> -->
+    <el-form :model="form"
+             ref="form"
+             :rules="rules"
+             size="mini"
+             label-position="left"
+             label-width="90px">
+      <el-row :gutter="10">
+        <el-col :span="12">
+          <el-form-item label="Type"
+                        prop="type">
+            <el-select v-model="form.type"
+                       clearable
+                       placeholder="select type"
+                       @change="handleTypeChange">
+              <el-option v-for="doc in typeOpts"
+                         :key="doc.value"
+                         :label="doc.label"
+                         :value="doc.value"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Student"
+                        prop="studentId">
+            <el-select v-model="form.studentId"
+                       @change="handleStudentChange"
+                       placeholder="Please Select Student">
+              <el-option v-for="doc in studentIdOpts"
+                         :key="doc.value"
+                         :label="doc.label"
+                         :value="doc.value"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Pay Date"
+                        prop="payDate">
+            <el-date-picker style="width:100%"
+                            v-model="form.payDate"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="Duration"
+                        prop="duration">
+            <el-select v-model="form.duration">
+              <el-option v-for="doc in 12 "
+                         :key="doc"
+                         :label="doc + ' months'"
+                         :value="doc"></el-option>
+            </el-select>
+          </el-form-item>
+
+        </el-col>
+        <el-col :span="12">
+          <fieldset>
+            <legend class="legend-style">Info</legend>
+            <el-row>
+              <el-col :span="12">
+                <el-form-item class="info"
+                              label="Late Date :">
+                  <label> {{ 0 }}</label>
+                </el-form-item>
+                <el-form-item class="info"
+                              label="End Date :">
+                  <label>{{ formatDate(endDate) }}</label>
+                </el-form-item>
+                <el-form-item class="info"
+                              label="Room :">
+                  <label>{{001}}</label>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item class="info"
+                              label="Teacher :">
+                  <label> {{ 0 }}</label>
+                </el-form-item>
+                <el-form-item class="info"
+                              label="Subject :">
+                  <label>{{ 0 }}</label>
+                </el-form-item>
+                <el-form-item class="info"
+                              label="Time :">
+                  <label>{{001}}</label>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </fieldset>
+        </el-col>
+      </el-row>
+      <el-form :model="formPayment"
+               :rules="rulesPayment"
+               ref="formPayment"
                size="mini"
-               label-position="left"
-               label-width="90px">
-        <el-row :gutter="10">
-          <el-col :span="8">
-            <el-form-item label="Type"
-                          prop="type">
-              <el-select v-model="form.type"
-                         clearable
-                         placeholder="select type"
-                         @change="handleTypeChange">
-                <el-option v-for="doc in typeOpts"
-                           :key="doc.value"
-                           :label="doc.label"
-                           :value="doc.value"></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="Subject"
-                          prop="classId">
-              <el-select v-model="form.classId"
-                         clearable
-                         placeholder="Select Class">
-                <el-option v-for="doc in classIdOpts"
-                           :key="doc.value"
-                           :label="doc.label"
-                           :value="doc.value">
-                  <span style="float: left">{{ doc.label }}</span>
-                  <span style="float: right; color: #8492a6; font-size: 13px">{{ doc.labelRight }}</span>
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="7">
-            <el-form-item>
-              <el-button type="primary"
-                         @click="handleSubmit">Submit</el-button>
-              <el-button @click="handleBack"
-                         icon="fa fa-arrow-circle-left"
-                         type="danger"> Back</el-button>
-            </el-form-item>
-          </el-col>
+               label-position="top"
+               :inline="true">
+        <fieldset>
+          <legend>Payment</legend>
 
-        </el-row>
+          <el-form-item label="Total Pay"
+                        prop="totalPay">
+            <el-input type="number"
+                      v-model="formPayment.totalPay"
+                      placeholder=""></el-input>
+          </el-form-item>
+          <el-form-item label="Discount"
+                        prop="discount">
+            <el-input type="number"
+                      style="width:21vh"
+                      v-model="formPayment.discount"
+                      placeholder=""></el-input>
+          </el-form-item>
+          <el-form-item label="USD"
+                        prop="usd">
+            <el-input type="number"
+                      v-model="formPayment.usd"></el-input>
+          </el-form-item>
+          <el-form-item label="KHR"
+                        prop="khr">
+            <el-input type="number"
+                      v-model="formPayment.khr"></el-input>
+          </el-form-item>
+          <el-form-item label="Remaining"
+                        prop="remaining">
+            <el-input type="number"
+                      v-model="formPayment.remaining"></el-input>
+          </el-form-item>
+        </fieldset>
       </el-form>
-    </fieldset>
-    <data-tables :data="dataTable"
-                 :action-col-def="actionColDef"
-                 :table-props="tableProps"
-                 v-loading="loading">
-      <el-table-column v-for="title in titles"
-                       :key="title.prop"
-                       :prop="title.prop"
-                       :label="title.label"
-                       :sortable="title.sort">
-        <template slot-scope="scope">
-          <span v-if="title.prop === 'payDate'">
-            {{ formatDate(scope.row.payDate) }}
-          </span>
-          <span v-else-if="title.prop === 'endPayDate'">
-            {{ formatDate(scope.row.endPayDate) }}
-          </span>
-          <span v-else>{{ scope.row[title.prop] }}</span>
-
-        </template>
-      </el-table-column>
-    </data-tables>
+      <el-form-item>
+        <el-button type="primary"
+                   @click="handleSubmit">Submit</el-button>
+        <el-button @click="handleBack"
+                   icon="fa fa-arrow-circle-left"
+                   type="danger"> Back</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -102,44 +160,47 @@ export default {
       modalVisible: false,
       modalDoc: null,
       loading: false,
-      classIdOpts: [],
+      studentIdOpts: [],
       classId: '',
+      endDate: moment().toDate(),
       form: {
         classId: '',
+        payDate: moment().toDate(),
+        duration: 1,
+        type: '',
+        studentId: '',
+      },
+      formPayment: {
+        totalPay: 0,
+        discount: 0,
+        usd: 0,
+        khr: 0,
+        remaining: 0,
       },
       rules: {
-        classId: [
-          { required: true, message: 'Subject is Required', trigger: 'change' },
+        studentId: [
+          { required: true, message: 'Student is Required', trigger: 'change' },
         ],
         type: [
           { required: true, message: 'Type is Required', trigger: 'change' },
         ],
-      },
-      dataTable: [],
-      titles: [
-        { label: 'Student', prop: 'student' },
-        { label: 'Gender', prop: 'gender' },
-        { label: 'Pay Date', prop: 'payDate' },
-        { label: 'End Date', prop: 'endPayDate' },
-        { label: 'Type', prop: 'type' },
-      ],
-      tableProps: {
-        size: 'small',
-      },
-      actionColDef: {
-        label: 'Action',
-        def: [
+        payDate: [
+          { required: true, message: 'Date is Required', trigger: 'change' },
+        ],
+        duration: [
           {
-            type: 'primary',
-            name: 'Pay',
-            buttonProps: {
-              size: 'mini',
-            },
-            handler: row => {
-              this.modalDoc = row
-              this.modalVisible = true
-              this.currentModal = StudentPay
-            },
+            required: true,
+            message: 'Duration is Required',
+            trigger: 'change',
+          },
+        ],
+      },
+      rulesPayment: {
+        totalPay: [
+          {
+            equired: true,
+            message: 'Total Pay is Required',
+            trigger: 'change',
           },
         ],
       },
@@ -191,48 +252,72 @@ export default {
     //     })
     // },
     handleTypeChange(val) {
-      if (val.length > 0) {
-        let selector = {
-          type: val,
-        }
-        lookupClass
-          .callPromise({ selector })
-          .then(result => {
-            // console.log(result)
-            this.classIdOpts = result
-          })
-          .catch(error => {
-            Notify.error({ message: error })
-          })
-      } else {
-        this.form.classId = ''
-        this.classIdOpts = []
+      let selector = {
+        type: val,
+        status: 'Debt',
+        // classId: this.form.classId,
+        // status: { $in: ['Expires', '$classDetail'] },
       }
+      this.loading = true
+      findPaymentForClass
+        .callPromise({ selector: selector })
+        .then(result => {
+          if (result.length > 0) {
+            this.dataTable = []
+            _.forEach(result[0].classDetail, o => {
+              this.studentIdOpts.push({
+                label: o.studentId + ' - ' + o.student,
+                value: o.studentId,
+              })
+            })
+          } else {
+            this.studentIdOpts = []
+          }
+          this.loading = false
+        })
+        .catch(error => {
+          Notify.error({ message: error.reason })
+        })
+    },
+    handleStudentChange(val) {
+      let selector = {
+        // type: val,
+        studentId: val,
+        // classId: this.form.classId,
+        // status: { $in: ['Expires', '$classDetail'] },
+      }
+      this.loading = true
+      findPaymentForClass
+        .callPromise({ selector: selector })
+        .then(result => {
+          console.log(result)
+          if (result.length > 0) {
+            // this.dataTable = []
+            // _.forEach(result[0].classDetail, o => {
+            //   this.studentIdOpts.push({
+            //     label: o.studentId + ' - ' + o.student,
+            //     value: o.studentId,
+            //   })
+            // })
+          } else {
+            this.studentIdOpts = []
+          }
+          this.loading = false
+        })
+        .catch(error => {
+          Notify.error({ message: error.reason })
+        })
     },
     handleSubmit() {
       this.$refs['form'].validate(valid => {
         if (valid) {
-          let selector = {
-            classId: this.form.classId,
-            status: { $in: ['Expires', '$classDetail'] },
-          }
-          this.loading = true
-          findPaymentForClass
-            .callPromise({ selector: selector })
-            .then(result => {
-              if (result.length > 0) {
-                this.dataTable = []
-                _.forEach(result[0].classDetail, o => {
-                  this.dataTable = result[0].classDetail
-                })
-              } else {
-                this.dataTable = []
-              }
-              this.loading = false
-            })
-            .catch(error => {
-              Notify.error({ message: error.reason })
-            })
+          this.$refs['formPayment'].validate(valid => {
+            if (valid) {
+              console.log(this.formPayment)
+            } else {
+              return false
+            }
+          })
         } else {
           return false
         }
@@ -254,8 +339,37 @@ export default {
     },
   },
 }
+// let selector = {
+//   classId: this.form.classId,
+//   status: { $in: ['Expires', '$classDetail'] },
+// }
+// this.loading = true
+// findPaymentForClass
+//   .callPromise({ selector: selector })
+//   .then(result => {
+//     if (result.length > 0) {
+//       this.dataTable = []
+//       _.forEach(result[0].classDetail, o => {
+//         this.dataTable = result[0].classDetail
+//       })
+//     } else {
+//       this.dataTable = []
+//     }
+//     this.loading = false
+//   })
+//   .catch(error => {
+//     Notify.error({ message: error.reason })
+//   })
 </script>
 
-<style>
-
+<style lang="scss" scoped >
+.legend-style {
+  font-weight: 400;
+  color: red;
+  font-size: 18px;
+}
+.info {
+  margin-bottom: 1vh;
+  font-weight: 400;
+}
 </style>
