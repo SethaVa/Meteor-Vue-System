07c08f082b6cde50ff3e6763623851af87a217f2
Navyroot <vyubbit@gmail.com>
2018-06-29 20:46:08 +0700

    fixture data

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,49 +1,28 @@
-import {
-  Meteor
-} from 'meteor/meteor'
-import {
-  Roles
-} from 'meteor/alanning:roles'
-import {
-  Accounts
-} from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor'
+import { Roles } from 'meteor/alanning:roles'
+import { Accounts } from 'meteor/accounts-base'
 
 import Branches from '../../api/branches/branches'
 // import Groups from '../../api/user-groups/user-groups';
 
-if (Meteor.isDevelopment) {
-  // Company
-  // if (Company.find().count() === 0) {
-  //     const data = {
-  //         "khName": "បាត់ដំបង",
-  //         "khShortName": "បប",
-  //         "enName": "Battambang",
-  //         "enShortName": "BTB",
-  //         "khAddress": "ខេត្តបាត់ដំបង",
-  //         "enAddress": "Battambang Province",
-  //         "telephone": "053 50 66 777",
-  //         "email": "yuom.theara@gmail.com",
-  //         "website": "http://rabbit-tech.com"
-  //     };
-
-  //     Company.insert(data);
-  // }
-
+Meteor.startup(function() {
   // Branch
   if (Branches.find().count() == 0) {
-    const data = [{
-      // name: 'Banan',
-      // shortName: 'BN',
-      // address: 'Battambang Province',
-      // telephone: '053 50 66 777',
-      // email: 'banan@gmail.com',mon
-      _id: '001',
-      name: 'Head Office',
-      shortName: 'HO',
-      address: 'Battambang Province',
-      telephone: '08757645',
-      email: 'admin@gmail.com',
-    }, ]
+    const data = [
+      {
+        // name: 'Banan',
+        // shortName: 'BN',
+        // address: 'Battambang Province',
+        // telephone: '053 50 66 777',
+        // email: 'banan@gmail.com',mon
+        _id: '001',
+        name: 'Head Office',
+        shortName: 'HO',
+        address: 'Battambang Province',
+        telephone: '08757645',
+        email: 'admin@gmail.com',
+      },
+    ]
 
     data.forEach(doc => {
       Branches.insert(doc)
@@ -51,38 +30,33 @@ if (Meteor.isDevelopment) {
   }
 
   // User
-  const users = [{
-      username: "super",
-      email: "super@super.com",
-      password: "123456",
+  const users = [
+    {
+      username: 'super',
+      email: 'super@super.com',
+      password: '123456',
       profile: {
-        fullName: "Super",
-        branchPermissions: ["001"]
+        fullName: 'Super',
+        branchPermissions: ['001'],
       },
-      roles: "super"
+      roles: 'admin',
     },
     {
-      username: "admin",
-      email: "admin@admin.com",
-      password: "123456",
+      username: 'admin',
+      email: 'admin@admin.com',
+      password: '123456',
       profile: {
-        fullName: "Admin",
-        branchPermissions: ["001"]
+        fullName: 'Admin',
+        branchPermissions: ['001'],
       },
-      roles: "admin"
-    }
+      roles: 'admin',
+    },
   ]
 
   // User
   if (Meteor.users.find().count() === 0) {
     // const data = EJSON.parse(Assets.getText('user-account.json'))
-    users.forEach(({
-      username,
-      email,
-      password,
-      profile,
-      roles
-    }) => {
+    users.forEach(({ username, email, password, profile, roles }) => {
       const userExists = Accounts.findUserByUsername(username)
 
       if (!userExists) {
@@ -96,4 +70,4 @@ if (Meteor.isDevelopment) {
       }
     })
   }
-}
+})
