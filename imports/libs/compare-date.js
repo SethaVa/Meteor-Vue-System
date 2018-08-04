9e48fc08c5e9af022d103145b6dcd2c44a811303
Navyroot <vyubbit@gmail.com>
2018-08-04 20:37:13 +0700

    refectore cpmare date

--- imports/libs/compare-date.js
+++ imports/libs/compare-date.js
@@ -1,25 +1,48 @@
 import _ from 'lodash'
 import moment from 'moment'
 import Payment from '../api/payment/payment'
-import { findPayment, updatePaymentStatus } from '../api/payment/methods'
+import {
+  findPayment,
+  updatePaymentStatus
+} from '../api/payment/methods'
 
-const compareDate = function() {
-  const currentDate = moment(moment().toDate()).format('L')
+const compareDate = function () {
+  // const currentDate = moment(moment().toDate()).format('L')
+  // const currentDate = wrapCurrentTime(moment(moment().toDate()).add(3, 'day'));
+  let currentDate = moment(moment().toDate()).add(3, 'day')
+  let current = moment()
+
+  currentDate.hour(current.hour())
+  currentDate.minute(current.minute())
+  currentDate.second(current.second())
+  currentDate.millisecond(current.millisecond())
+  currentDate = currentDate.toDate()
+
   let selector = {
     status: 'Paid',
+    endPayDate: {
+      $lte: moment(currentDate)
+        .endOf('day')
+        .toDate(),
+    },
   }
   findPayment
-    .callPromise({ selector: selector })
+    .callPromise({
+      selector: selector
+    })
     .then(result => {
       _.forEach(result, obj => {
-        let compareDate = moment(currentDate).isSame(
-          moment(obj.endPayDate).format('L')
-        )
+        //   let compareDate = moment(currentDate).isSame(
+        //     moment(obj.endPayDate).format('L')
+        //   )
 
         if (compareDate) {
           let value = 'Expires'
           updatePaymentStatus
-            .callPromise({ _id: obj._id, value })
+            .callPromise({
+              _id: obj._id,
+              value
+            })
             .then(result => {})
             .catch(error => {})
         }
